c122f7be8e5f59bf7b8838fb0ff7208a10084191
ram <ram30sahu@gmail.com>
2018-02-18 22:31:57 +0530

    testrepo project

---
+++ src/main/java/com/nt/service/WishMsgGenerator.java
@@ -0,0 +1,26 @@
+package com.nt.service;
+
+import java.util.Calendar;
+
+import org.springframework.stereotype.Service;
+@Service("wish")
+public class WishMsgGenerator {
+
+	public String generateWishMsg(String name){
+		int hour=0;
+		Calendar calander=null;
+		calander=Calendar.getInstance();
+        hour=calander.get(Calendar.HOUR_OF_DAY);
+        
+        	if(hour<=12)
+   			 return "GM:"+name;
+   		 else if(hour<=16)
+   			 return "GA:"+name;
+   		 else if(hour<=20)
+   			 return "GE   "+name;
+   		 else 
+   			 return "GN:"+name;
+		
+   	}//method
+   }//class
+
---
+++ src/main/java/com/nt/test/BootProj1CoreDiApplication.java
@@ -0,0 +1,24 @@
+package com.nt.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+
+import com.nt.service.WishMsgGenerator;
+@SpringBootApplication
+@ComponentScan(basePackages="com.nt.service")
+public class BootProj1CoreDiApplication {
+
+	public static void main(String[] args) {
+		ApplicationContext ctx=null;
+		WishMsgGenerator generator=null;
+		//create IOC container
+		ctx=SpringApplication.run(BootProj1CoreDiApplication.class, args);
+      //get bean
+       generator=ctx.getBean("wish",WishMsgGenerator.class);
+       System.out.println(generator.generateWishMsg("raja"));
+	
+	}
+
+}
